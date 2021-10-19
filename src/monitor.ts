55a1593859a439d4ee04f6339089008c8b28b39d
Chuck Kato <chuck.kato@coursehero.com>
2021-10-19 12:10:34 -0700

    Try to improve the code

--- src/monitor.ts
+++ src/monitor.ts
@@ -11,24 +11,43 @@ if( !infuraKey ) {
     process.exit(1);
 }
 
-// Initialize 
-let web3 = new Web3(
-  new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/" + infuraKey)
-);
+function start() {
+    // Initialize web3
+    let provider = new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/" + infuraKey)
+    let web3 = new Web3(provider);
 
-const contract = new web3.eth.Contract(abi, address);
+    const contract = new web3.eth.Contract(abi, address);
 
-// Monitor new events
-contract.events.Transfer(
-    { fromBlock: "latest" },
-    (errors:any, events:any) => {
-        if (!errors) {
-            for(const transfer of events ) {
-                const tokenId = transfer.returnValues.tokenId;
-                const prevOwner = transfer.returnValues.from;
-                const newOwner = transfer.returnValues.to;
-                console.log("Token ID " + tokenId + " has been transfered from " + prevOwner + " to " + newOwner);
-            }
-        }
-    }
-);
+    // Monitor new events
+    contract.events.Transfer({ fromBlock: "latest" })
+    .on("connected", (subscriptionId:any) => {
+        console.log("connected");
+        console.log(subscriptionId);
+    })
+    .on("data", (event:any) => {
+        console.log("data");
+        const tokenId = event.returnValues.tokenId;
+        const prevOwner = event.returnValues.from;
+        const newOwner = event.returnValues.to;
+        console.log("Token ID " + tokenId + " has been transfered from " + prevOwner + " to " + newOwner);
+    })
+    .on("changed", (event:any) => {
+        console.log("changed");
+        // remove event from local database
+    })
+    .on("error", (error:any, receipt:any) => {
+        // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+        console.log("error");
+        console.log(error, receipt);
+        console.log("restarting web3");
+        start();
+    })
+    .on("end", () => {
+        console.log("end");
+        console.log("restarting web3");
+        start();
+    });
+}
+
+console.log("starting web3");
+start();
